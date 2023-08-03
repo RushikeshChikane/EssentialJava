1421161860be64af2f4eb9afd89428a1a789cd2f
RushikeshChikane <rushikeshchikane101@gmail.com>
2023-08-03 21:54:12 +0530

    CRUDS

---
+++ 1__8_2023/day19_EMS_backend/src/main/java/com/app/pojos/Employee.java
@@ -0,0 +1,33 @@
+package com.app.pojos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="emp")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonProperty("id")
+	private Integer empId;
+	@Column(length = 30)
+	private String name;
+	@Column(length = 30)
+	private String department;
+	@JsonProperty("location")
+	@Column(length = 30)
+	private String workLocation;
+}
